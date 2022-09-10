a083331ad51a0e75acf774549b795396799d825e
Precious Emerole <preciousemerole@gmail.com>
2022-09-10 14:18:01 -0700

    task 10 modified

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,17 +15,19 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 48; n < 57; n++)
+	for (n = 48; n < 58; n++)
 	{
-		for (m = 48; m < 57; m++)
+		for (m = 49; m < 58; m++)
 		{
-			putchar(n);
-			putchar(m);
-			if (n != 57 || m != 57)
+			if (n > m)
 			{
-				putchar(',');
-				putchar(' ');
-
+				putchar(n);
+				putchar(m);
+				if (n != 55 || m != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}

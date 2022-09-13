f518468d9c3acec49bd59e78b0829098381cd088
Precious Emerole <preciousemerole@gmail.com>
2022-09-13 15:34:53 -0700

    task 8 code modified

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int i;
+	int j;
 
-	i = 0;
-
-	while (i < 24)
+	for (i = 0; i < 24; i++)
 	{
-		while (j < 60)
+		for (j = 0; j < 59; j++)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
@@ -19,8 +17,6 @@ void jack_bauer(void)
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
-			j++;
 		}
-		i++;
 	}
 }

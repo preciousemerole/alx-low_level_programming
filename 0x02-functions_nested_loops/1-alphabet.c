eeb506943fb2e715e0c7dedb3028463d144be7a2
Precious Emerole <preciousemerole@gmail.com>
2022-09-13 13:35:23 -0700

    task 1 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line
+ * Return: Always 0;
+ */
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

acd161f058e14c17007f01b345b1412037253de5
Precious Emerole <preciousemerole@gmail.com>
2022-09-13 06:38:00 -0700

    testing code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - prints _putchar followed by a new line
  * Return: 0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	putchar("_putchar\n");
 	return (0);
 }

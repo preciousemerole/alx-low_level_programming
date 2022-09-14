d5ed428da9475b01cc2e11e7a4de1c083ee7687f
Precious Emerole <preciousemerole@gmail.com>
2022-09-13 20:43:29 -0700

    task 9 done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,5 +16,20 @@ void times_table(void)
 			if (j == 0)
 			{
 				_putchar('0');
+			} else if (prod <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			} else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
 			}
+		}
+		_putchar('\n');
+	}
 }

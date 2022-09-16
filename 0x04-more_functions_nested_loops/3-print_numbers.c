2425e60c3025fd663a3546588d6f10f13966d2e5
Precious Emerole <preciousemerole@gmail.com>
2022-09-16 10:27:46 -0700

    task 3 corrected

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,14 @@
 /**
  * print_numbers - prints the numbers, from zero to nine,
  * followed by a new line
- * Return: Always 0 (Success)
  */
 void print_numbers(void)
 {
 	int c;
 
-	for (c >= '0' && c <= '9')
+	for (c >= '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
 }

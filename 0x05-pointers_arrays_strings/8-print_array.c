a69c470c9cdaada2f25622f79ce56b047e716178
Precious Emerole <preciousemerole@gmail.com>
2022-09-19 15:51:35 -0700

    task 8 again ...

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,9 +4,10 @@
 /**
  * print_array -  prints n elements of an array of integers,
  * followed by a new line.
- * where n is the number of elements of the array to be printed
  * Numbers must be separated by comma, followed by a space
  * and displayed in the same order as they are stored in the array
+ * @a: arrays to be printed
+ * @n: number of elements to be printed
  */
 void print_array(int *a, int n)
 {
@@ -16,7 +17,7 @@ void print_array(int *a, int n)
 	{
 		if (i == 0)
 			printf("%d", a[i]);
-		else 
+		else
 			printf(", %d", a[i]);
 	}
 	printf("\n");

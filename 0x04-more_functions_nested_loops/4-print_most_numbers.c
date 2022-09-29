69abadd369989814beb276035574bc1aa207a6de
Precious Emerole <preciousemerole@gmail.com>
2022-09-29 04:31:38 -0700

    Task 0 done and checked

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * print_most_numbers - prints number from zero to nine, except two and four
- * followed by a new line
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line.
  */
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = '0'; i < '10'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i =! '2' && i != '4')
+		if (i != 2 && i != 4)
 			_putchar(i + '0');
 	}
 	_putchar('\n');
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *infinite_add - adds two numbers
+ * @n1: the first number
+ * @n2: the second number
+ * @r: buffer that the function will use to store result
+ * @size_r: buffer size
+ *
+ * Return: a pointer to the result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+}

b06069c4d657a0b84d7ef04729ab8e294fe6a2f5
Precious Emerole <preciousemerole@gmail.com>
2022-10-07 05:09:37 -0700

    changed code of task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,35 +17,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, len1, len2;
 	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s2 == NULL)
-		s2 = "";
-	if (s1 == NULL)
-		s1 = "";
-
-	len1 = 0;
-	len2 = 0;
-	while (s2[len2] != '\0')
-		len2++;
-	while (s1[len1] != '\0')
+	while (s1 && s1[len])
 		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	if (n >= len2)
-		n = len2;
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	s = malloc(sizeof(char) * n + len1 + 1);
-	if (s == NULL)
+	if (!s)
 		return (NULL);
 
-	for (i = 0; i < len1; i++)
+	while (i < len1)
+	{
 		s[i] = s1[i];
+		i++;
+	}
 
-	for (i = 0; i < n; n++)
-		s[i + len1] = s2[i];
+	while (n < len2 && i < (len1 + n))
+		s1[i++] = s2[j++];
+	while (n >= len2 && i < (len1 + n))
+		s1[i++] = s2[j++];
 
-	s[i + len1] = '\0';
+	s[i] = '\0';
 
 	return (s);
 }
